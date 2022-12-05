c363ff88f5564c3ce392c562cd778be9b7ae1fdf
Kharzette <6589537+Kharzette@users.noreply.github.com>
2022-12-05 07:31:24 -0600

    Basic window creation with SDL
    The dxvk stuff might be kind of unfriendly to C, we shall C, I mean see

--- UtilityLib/GraphicsDevice.c
+++ UtilityLib/GraphicsDevice.c
@@ -1,3 +1,44 @@
 #include	<d3d11.h>
+#include	<SDL3/SDL.h>
+#include	<stdint.h>
+#include	<stdio.h>
 
 ID3D11Device	sDevice;
+
+
+int main(void)
+{
+	printf("DirectX on looney loonix!\n");
+
+	SDL_Window	*pWnd	=NULL;
+
+	int	res	=SDL_Init(SDL_INIT_VIDEO);
+	if(res < 0)
+	{
+		printf("SDL init failed: %s\n", SDL_GetError());
+		return	0;
+	}
+
+	pWnd	=SDL_CreateWindow("Blortallius!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+	if(pWnd == NULL)
+	{
+		printf("Window creation didn't work: %s\n", SDL_GetError());
+		SDL_Quit();
+		return	0;
+	}
+
+	printf("Huzzah!\n");
+
+	sleep(5);
+
+	SDL_DestroyWindow(pWnd);
+
+	SDL_Quit();
+
+	return	1;
+}
+
+//int	SDL_main(int argc, char *argv[])
+//{
+//	SDL_Init()
+//}

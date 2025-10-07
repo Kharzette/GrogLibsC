466b7d523b7e490d994459d699d626871b8863ba
Kharzette <6589537+Kharzette@users.noreply.github.com>
2025-10-07 02:48:18 -0500

    Some basic checks for build needed.  Donut think it looks at headers, just c or cpp source.  Well and the target file.

--- BuildIt.c
+++ BuildIt.c
@@ -4,6 +4,66 @@
 #include	<stdlib.h>
 
 
+//static forward decs
+static bool	sBuildLib(const char *szPath, bool bCpp);
+static bool	sBuildCLib(const char *szPath);
+static bool	sBuildCppLib(const char *szPath);
+static int	sNeedsBuild(const char *szPath, bool bCpp);
+static bool	sbIsC(const char *szFileName);
+static bool	sbIsCpp(const char *szFileName);
+static void	sStandardLibJunk_Add(Nob_Cmd *pCmd);
+
+
+//not really doing anything with command line args yet
+int	main(int argc, char **argv)
+{
+	NOB_GO_REBUILD_URSELF(argc, argv);
+
+	//ensure libs dir there
+	if(!nob_mkdir_if_not_exists("libs"))
+	{
+		printf("Something went wrong making the libs dir...\n");
+		return	EXIT_FAILURE;
+	}
+
+	if(!sBuildLib("UtilityLib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("UILib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("TerrainLib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("PhysicsLib", true))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("MeshLib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("MaterialLib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("InputLib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	if(!sBuildLib("AudioLib", false))
+	{
+		return	EXIT_FAILURE;
+	}
+	
+	return	EXIT_SUCCESS;
+}
+
+
+//statics
 static void	sStandardLibJunk_Add(Nob_Cmd *pCmd)
 {
 	//stuff all the libraries like
@@ -46,7 +106,64 @@ static bool	sbIsCpp(const char *szFileName)
 		&& szFileName[len - 1] == 'p');
 }
 
-static bool sBuildPhysicsLib(const char *szPath)
+//returns < 0 if error, 0 if no build needed
+static int sNeedsBuild(const char *szPath, bool bCpp)
+{
+	//cd to dir
+	if(!nob_set_current_dir(szPath))
+	{
+		printf("Something went wrong changing to lib dir %s\n", szPath);
+		return	false;
+	}
+
+	//make the wierdly named so file name
+	char	tmp[128];
+	sprintf(tmp, "../libs/lib%s.so", szPath);
+
+	//grab all the source files
+    Nob_File_Paths	cFiles		={0};
+	Nob_File_Paths	allFiles	={0};
+	if(!nob_read_entire_dir(".", &allFiles))
+	{
+		printf("Something went wrong reading dir %s\n", szPath);
+		return	false;
+	}
+
+	//add all the source files
+	for(int i=0;i < allFiles.count;i++)
+	{
+		bool	bIsSource;
+		if(bCpp)
+		{
+			bIsSource	=sbIsCpp(allFiles.items[i]);
+		}
+		else
+		{
+			bIsSource	=sbIsC(allFiles.items[i]);
+		}
+
+		if(bIsSource)
+		{
+			nob_da_append(&cFiles, allFiles.items[i]);
+		}
+	}
+
+	nob_da_free(allFiles);
+
+	int	buildNeeded	=nob_needs_rebuild(tmp, cFiles.items, cFiles.count);
+
+	nob_da_free(cFiles);
+	
+	//pop back up a dir
+	if(!nob_set_current_dir(".."))
+	{
+		printf("Something went wrong changing to ..\n");
+		return	-1;
+	}
+	return	buildNeeded;
+}
+
+static bool sBuildCppLib(const char *szPath)
 {
 	Nob_Cmd	libCmd	={0};
 
@@ -93,7 +210,7 @@ static bool sBuildPhysicsLib(const char *szPath)
 	nob_cmd_append(&libCmd,
 		tmp, "../JoltPhysics/Build/Linux_Debug/libJolt.a");
 
-	if(!nob_cmd_run(&libCmd))	return	false;
+	bool	bWorked	=nob_cmd_run(&libCmd);
 
 	//pop back up a dir
 	if(!nob_set_current_dir(".."))
@@ -105,7 +222,7 @@ static bool sBuildPhysicsLib(const char *szPath)
 	nob_da_free(cppFiles);
 	nob_cmd_free(libCmd);
 	
-	return	true;
+	return	bWorked;
 }
 
 static bool	sBuildCLib(const char *szPath)
@@ -118,10 +235,10 @@ static bool	sBuildCLib(const char *szPath)
 		printf("Something went wrong changing to lib dir %s\n", szPath);
 		return	false;
 	}
-	
+
 	sStandardLibJunk_Add(&libCmd);
 
-    Nob_File_Paths	cFiles	={0};
+    Nob_File_Paths	cFiles		={0};
 
 	if(!nob_read_entire_dir(".", &cFiles))
 	{
@@ -132,7 +249,6 @@ static bool	sBuildCLib(const char *szPath)
 	//add all the .c files
 	for(int i=0;i < cFiles.count;i++)
 	{
-//		printf("Filez:%s \n", cFiles.items[i]);
 		if(sbIsC(cFiles.items[i]))
 		{
 			nob_cmd_append(&libCmd, cFiles.items[i]);
@@ -151,12 +267,13 @@ static bool	sBuildCLib(const char *szPath)
 	//make the wierdly named so file name
 	char	tmp[128];
 	sprintf(tmp, "../libs/lib%s.so", szPath);
+
 	nob_cmd_append(&libCmd,
 		tmp, "-lvulkan", "-lpng16", "-lSDL3",
 		"../dxvk-native/build/src/dxgi/libdxvk_dxgi.so",
 		"../dxvk-native/build/src/d3d11/libdxvk_d3d11.so");
 
-	if(!nob_cmd_run(&libCmd))	return	false;
+	bool	bWorked	=nob_cmd_run(&libCmd);
 
 	//pop back up a dir
 	if(!nob_set_current_dir(".."))
@@ -168,52 +285,28 @@ static bool	sBuildCLib(const char *szPath)
 	nob_da_free(cFiles);
 	nob_cmd_free(libCmd);
 	
-	return	true;
+	return	bWorked;
 }
 
-int	main(int argc, char **argv)
+static bool	sBuildLib(const char *szPath, bool bCpp)
 {
-	NOB_GO_REBUILD_URSELF(argc, argv);
-
-	//ensure libs dir there
-	if(!nob_mkdir_if_not_exists("libs"))
+	int	needsBuild	=sNeedsBuild(szPath, bCpp);
+	if(needsBuild < 0)
 	{
-		printf("Something went wrong making the libs dir...\n");
-		return	EXIT_FAILURE;
+		return	false;	//error
+	}
+	else if(!needsBuild)
+	{
+		printf("No build needed for %s...\n", szPath);
+		return	true;	//no build needed
 	}
 
-	if(!sBuildCLib("UtilityLib"))
+	if(bCpp)
 	{
-		return	EXIT_FAILURE;
+		return	sBuildCppLib(szPath);
 	}
-	if(!sBuildCLib("UILib"))
+	else
 	{
-		return	EXIT_FAILURE;
+		return	sBuildCLib(szPath);
 	}
-	if(!sBuildCLib("TerrainLib"))
-	{
-		return	EXIT_FAILURE;
-	}
-	if(!sBuildPhysicsLib("PhysicsLib"))
-	{
-		return	EXIT_FAILURE;
-	}
-	if(!sBuildCLib("MeshLib"))
-	{
-		return	EXIT_FAILURE;
-	}
-	if(!sBuildCLib("MaterialLib"))
-	{
-		return	EXIT_FAILURE;
-	}
-	if(!sBuildCLib("InputLib"))
-	{
-		return	EXIT_FAILURE;
-	}
-	if(!sBuildCLib("AudioLib"))
-	{
-		return	EXIT_FAILURE;
-	}
-	
-	return	EXIT_SUCCESS;
 }
